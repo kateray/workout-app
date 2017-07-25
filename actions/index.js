b6a422d1222ef18981f0f9707f64cd4fc0ddeadf
Kate Ray <kateray@users.noreply.github.com>
2017-07-25 11:06:15 -0400

    Add storage using redux-persist

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,19 @@
+function guid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
 import { ADD_CATEGORY } from '../constants'
+import { AsyncStorage } from 'react-native'
 
 export function addCategory(payload) {
+  payload = Object.assign({id: guid()}, payload)
   return {
     payload,
     type: ADD_CATEGORY
--- configureStore.js
+++ configureStore.js
@@ -1,7 +1,16 @@
-import { createStore } from 'redux'
+import { compose, createStore } from 'redux'
 import {app} from './reducers'
+import {persistStore, autoRehydrate} from 'redux-persist'
+import {AsyncStorage} from 'react-native'
 
 export const configureStore = () => {
-  let store = createStore(app)
+  let store = createStore(
+    app,
+    undefined,
+      compose(
+        autoRehydrate()
+      )
+  )
+  persistStore(store, {storage: AsyncStorage})
   return store
 }
