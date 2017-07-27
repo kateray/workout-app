f18ffc6d611bf93eaca26e705c9756cb453c1cc8
Kate Ray <kateray@users.noreply.github.com>
2017-07-27 14:44:27 -0400

    Made EditCategory more dumb

--- components/EditCategoryScreen.js
+++ components/EditCategoryScreen.js
@@ -1,20 +1,16 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { View, Button } from 'react-native'
 import { ExerciseList } from './ExerciseList'
 
-export class EditCategoryScreen extends PureComponent {
-  _openExerciseForm = (categoryId, exerciseName) => (
-    this.props.navigation.navigate('EditExercise', {categoryId: categoryId, name: exerciseName})
+export const EditCategoryScreen = ({navigation}) => {
+  const { navigate, state } = navigation
+  const openExerciseForm = (categoryId, exerciseName) => (
+    navigate('EditExercise', {categoryId: categoryId, name: exerciseName})
   )
-
-  render () {
-    // The screen's current route is passed in to `props.navigation.state`:
-    const { state } = this.props.navigation
-    return (
-      <View>
-        <Button title='Add Exercise' onPress={() => this._openExerciseForm(state.params.categoryId, false)} />
-        <ExerciseList categoryId={state.params.categoryId} editExercise={this._openExerciseForm} />
-      </View>
-    )
-  }
+  return (
+    <View>
+      <Button title='Add Exercise' onPress={() => openExerciseForm(state.params.categoryId, false)} />
+      <ExerciseList categoryId={state.params.categoryId} editExercise={openExerciseForm} />
+    </View>
+  )
 }
